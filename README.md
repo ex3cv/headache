9ddf19a8c1d3f56edb8011982afb6ad950688f43
Remigiusz Świc <remigiusz@swic.net.pl>
2021-12-23 22:47:56 +0100

    Add functional app

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-### Headache
+# Headache
 
-Simple app to printout http headers using asyncio
+Simple app to printout http request information using asyncio
+
+### Logging
+Nothing fancy. To enable logs in json format set env variable `HEADACHE_LOG_FORMATTER` to `json`.
--- headache/__main__.py
+++ headache/__main__.py
@@ -1,5 +1,50 @@
-def main():
-    pass
+import os
+import logging
+from pythonjsonlogger import jsonlogger
+
+from aiohttp import web
+
+
+def configure_logging():
+    logger = logging.getLogger()
+    logHandler = logging.StreamHandler()    
+
+    if os.getenv("HEADACHE_LOG_FORMATTER", "text").lower() == "json":
+        formatter = jsonlogger.JsonFormatter()
+        logHandler.setFormatter(formatter)
+
+    logger.addHandler(logHandler)
+    logger.setLevel(logging.DEBUG)
+
+
+
+async def print_headers(request: web.BaseRequest) -> web.Response:
+    headers = str()
+    for k, v in request.headers.items():
+        headers += f"{k}: {v}<br>"
+
+    response = (
+        "<html><head><title>Headache</title></head><body>"
+        f"<p>Request URL: {request.url}<br>"
+        f"Request method: {request.method}<br>"
+        f"Remote client IP address: {request.remote}</p>"
+        "<hr>"
+        "<p>Request headers:</p>"
+        f"<p>{headers}</p>"
+        "</body></html>"
+    )
+
+    return web.Response(text=response,
+                        content_type="text/html")
+
+
+def main() -> None:
+    configure_logging()
+    app = web.Application()
+    app.add_routes([web.get("/{tail:.*}", print_headers),
+                    web.post("/{tail:.*}", print_headers)])
+
+    web.run_app(app)
 
 
 if __name__ == "__main__":
